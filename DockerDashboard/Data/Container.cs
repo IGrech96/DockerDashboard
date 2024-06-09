c9b74d9643849e0a30c845c7a7332ff5f5000499
Ivan Grechko <ivang2896@gmail.com>
2024-06-09 12:39:41 +0300

    Add project files.

---
+++ DockerDashboard/Data/Container.cs
@@ -0,0 +1,29 @@
+﻿namespace DockerDashboard.Data;
+
+public class Container
+{
+    public string ContainerId { get; set; }
+
+    public string ContainerName { get; set; }
+
+    public string ImageName { get; set; }
+
+    public ContainerStatus Status { get; set; }
+
+    public string Created { get; set; }
+
+    public (int localPort, int containerPort)[] Ports { get; set; }
+}
+
+public enum ContainerStatus
+{
+    Starting,
+
+    Running,
+
+    Stoping,
+
+    Failed,
+
+    Stoped,
+}
