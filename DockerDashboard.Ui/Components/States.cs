425ff18e1034af36dff5599880fd68bcd03440a8
Ivan Grechko <ivang2896@gmail.com>
2024-08-11 08:51:32 +0300

    Fix sidebar layout

---
+++ DockerDashboard.Ui/Components/States.cs
@@ -0,0 +1,18 @@
+﻿namespace DockerDashboard.Ui.Components
+{
+    public class CommonInfo(string name, object value)
+    {
+        public string Name { get; set; } = name;
+
+        public object? Value { get; set; } = value;
+    }
+
+    public class DetailedInfo(string category, string name, object value)
+    {
+        public string Category { get; set; } = category;
+
+        public string Name { get; set; } = name;
+
+        public object? Value { get; set; } = value;
+    }
+}
