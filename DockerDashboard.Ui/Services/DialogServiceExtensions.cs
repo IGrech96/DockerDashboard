25f09d4ebfd07dee0c948e549be3889e9deebb83
Ivan Grechko <ivang2896@gmail.com>
2024-09-08 11:56:54 +0300

    DOC-11 Quick actions for containers

--- DockerDashboard.Ui/Services/DialogServiceExtensions.cs
+++ DockerDashboard.Ui/Services/DialogServiceExtensions.cs
@@ -1,12 +1,13 @@
-﻿using DockerDashboard.Shared.Hubs;
+﻿using DockerDashboard.Shared.Data;
+using DockerDashboard.Shared.Hubs;
 using DockerDashboard.Ui.Components;
 using Radzen;
+using Radzen.Blazor;
 
 namespace DockerDashboard.Ui.Services;
 
 public static class DialogServiceExtensions
 {
-
     public static Task<dynamic> OpenProgressDialogAsync(
         this Radzen.DialogService service, 
         string title,
@@ -15,5 +16,33 @@ public static class DialogServiceExtensions
         return service.OpenAsync<ProgressDialog>(
             title,
             new () { ["Action"] = action });
+    }
+
+    public enum ReCreateContainerDialogResult
+    {
+        Cancel,
+
+        Recreate,
+
+        RecreateWithNewImage
+    }
+
+    public static async Task<ReCreateContainerDialogResult> OpenRecreateContainerDialogAsync(
+        this DialogService service,
+        ContainerModel container)
+    {
+        Dictionary<string, object> data = new()
+        {
+            ["Container"] = container,
+            ["PullImage"] = false
+        };
+        var result = await service.OpenAsync<RecreateContainerDialog>("Re-Create container", data);
+
+        if (result is ReCreateContainerDialogResult enumResult)
+        {
+            return enumResult;
+        }
+
+        return ReCreateContainerDialogResult.Cancel;
     }
 }
